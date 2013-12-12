c9d849335c5b2fc7d2629df7ed14d49d8e6ec898
Kevin Jung <admin@kjung.ca>
2013-12-12 15:43:34 -0500

    updated to use grunt

---
+++ Gruntfile.js
@@ -0,0 +1,133 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    // sass
+    sass: {
+      build: {
+        options: {
+          style: 'nested'
+        },
+        src: ['assets/sass/styles.scss'],
+        dest: 'assets/css/styles.css'
+      }
+    },
+    autoprefixer: {
+      options: {
+
+      },
+      build: {
+        src: 'assets/css/styles.css'
+      },
+    },
+    cssmin: {
+      options: {
+        keepSpecialComments: true,
+      },
+      build: {
+        src: 'assets/css/*.css',
+        dest: 'assets/release/main.min.css'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      build: [
+        'Gruntfile.js',
+        'assets/js/init.js',
+      ]
+    },
+    // concat
+    concat: {
+      options: {
+        separator: ';',
+      },
+      // combine all js files into one
+      build: {
+        src: ['assets/js/bootstrap.min.js','assets/js/init.js'],
+        dest: 'assets/js/app.js',
+      },
+    },
+    uglify: {
+      options: {
+        mangle: false //To prevent changes to your variable and function names
+      },
+      build: {
+        files: {
+          'assets/release/app.min.js': 'assets/js/app.js'
+        }
+      }
+    },
+    // clean (delete files)
+    clean: {
+      release: [
+        'assets/css',
+        'assets/js',
+        'assets/sass',
+      ]
+    },
+    imagemin: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'assets/images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'assets/images/'
+        }]
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      html: {
+        files: ['index.html'],
+      },
+      sass: {
+        files: ['assets/sass/*.scss'],
+        tasks: ['sass', 'autoprefixer', 'cssmin'],
+      },
+      js: {
+        files: ['assets/js/init.js'],
+        tasks: ['jshint', 'conca', 'uglify']
+        },
+    },
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+
+  // Register tasks
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'sass',
+    'autoprefixer',
+    'cssmin',
+    'concat',
+    'uglify',
+    'imagemin',
+  ]);
+
+  grunt.registerTask('deploy', [
+    'sass',
+    'autoprefixer',
+    'cssmin',
+    'concat',
+    'uglify',
+    'imagemin',
+    'clean',
+  ]);
+
+};
--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-## Bootlace
+# Bootlace
 
-A ready to use html5 boilerplate including bootstrap 3.
+## Requirements
+* Ruby
+* Node
+* Grunt
+
+## Install
+ Start by installing `Grunt` and `Sass` by running these commands:
+
+```shell
+npm install -g grunt-cli
+gem install sass
+```
+
+Clone this repo:
+
+```shell
+git clone https://github.com/kjunggithub/bootlace
+cd bootlace
+```
+Install all the npm dependencies by running
+
+```shell
+npm install
+```
+## Grunt Workflow
+To build:
+
+```shell
+grunt build
+```
+To watch:
+
+```shell
+grunt watch
+```
+To deploy:
+
+```shell
+grunt deploy
+```
